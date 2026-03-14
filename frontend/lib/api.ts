401c2fc45c81eb5c8df4253668d4aff88a458526
Naren Maharjan <naren.14151415@gmail.com>
2026-03-14 11:53:04 +0545

    url update for deployment

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://job-preparation-ai-3.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
